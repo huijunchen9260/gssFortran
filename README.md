3c965b5fa9a7cf6e5e58361d30d343384e720115
huijunchen9260 <chen.9260@osu.edu>
2022-10-09 15:49:01 -0400

    Change back

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gssFortran
-My cool new project!
+
+This repo is to test on using `class(*)` to bring arbitrary objective function into the algorithm and compare its efficiency with directly code golden section search for specific optimization.
+
+My current conclusion is
+- if you need to put `configurations` derived type into the `func_data` that the coding is doing, then the array in derived type should be in the heap memory rather than stack memory. In fortran syntax, the array should be declared using `allocatable` but not assign array `shape` when defined in the derived type.
+
+
--- app/main.f90
+++ app/main.f90
@@ -3,25 +3,60 @@ program main
     use iso_Fortran_env, only: rk => real64, ik => int32
     use goldenSectionSearch
     implicit none
+    integer(ik), parameter :: bknum = 100
+    integer(ik), parameter :: knum = 100
+    integer(ik), parameter :: enum = 100
     real(rk) :: fout, xout, t0, t1
 
-    call cpu_time(t0)
-    call goldenSec(fout, xout, LB = -2.0_rk, UB = 7.0_rk)
-    call cpu_time(t1)
-    write(*, *) "subroutine gss took ", t1 - t0
+
+
+    type configurations
+        real(rk) :: addition
+        real(rk) :: zval                    ! TFP shock
+        real(rk) :: wval                    ! wage
+        real(rk) :: pval                    ! Marginal utility
+        real(rk) :: pfval                   ! future Marginal utility
+        real(rk) :: qbval                   ! bond price
+        real(rk) :: Qbuy                    ! capital purchasing price
+        real(rk) :: qsell                   ! capital selling price
+        real(rk) :: xuval                   ! cash on hand for upward-adj firm
+        real(rk) :: xdval                   ! cash on hand for downward-adj firm
+        ! real(rk) :: ewbk(bknum, knum)
+        real(rk), allocatable :: ewk(:), ewbk(:, :), evbk(:, :), evk(:)
+        ! real(rk) :: evbk(bknum, knum)
+        ! real(rk) :: evk(knum)
+    end type
+
+    type(configurations) :: conf
+
+    allocate(conf%ewk(knum), conf%ewbk(bknum, knum))
+
+    conf%addition = 2.0_rk
+    conf%qsell = 0.918790087890625_rk
+    conf%wval = 1.087975585937500_rk
+
+    deallocate(conf%ewk)
 
     call cpu_time(t0)
-    call gss(fout, xout, f, LB = -2.0_rk, UB = 7.0_rk, show = .false.)
+    call goldenSec(fout, xout, LB = -2.0_rk, UB = 7.0_rk, conf = conf)
     call cpu_time(t1)
-    write(*, *) "gss with class took ", t1 - t0
+    write(*, *) fout, xout
+    write(*, *) "subroutine gss took ", t1 - t0, " seconds"
+
+    call cpu_time(t0)
+    call gss(fout, xout, f, LB = -2.0_rk, UB = 7.0_rk, show = .false., func_data = conf)
+    call cpu_time(t1)
+    write(*, *) fout, xout
+    write(*, *) "gss with class took ", t1 - t0, " seconds"
 
 
 contains
 
-    subroutine goldenSec(fout, xout, LB, UB)
+    subroutine goldenSec(fout, xout, LB, UB, conf)
         real(rk), parameter :: goldenRatio = (3.0_rk - dsqrt(5.0_rk)) / 2.0_rk
         real(rk), intent(out) :: fout, xout
         real(rk), intent(in) :: LB, UB
+        type(configurations), intent(in), optional :: conf
         integer(ik) :: iter, gssMaxIter
         logical :: isFindMax, isShow
         character(len=128) :: tmp
@@ -39,8 +74,8 @@ contains
         c = a + goldenRatio*(b-a);
         d = a + (1-goldenRatio)*(b-a);
 
-        fval = -c**2; fc = -fval
-        fval = -d**2; fd = -fval
+        fval = -c**2 + conf%addition; fc = -fval
+        fval = -d**2 + conf%addition; fd = -fval
 
         do while (gssDist > gssTol .and. iter <= gssMaxIter)
 
@@ -54,7 +89,7 @@ contains
                 fc = fd
                 d = z
                 ! fval = Obj(d, func_data); fd = merge(-fval, fval, isFindMax);
-                fval = -d**2; fd = -fval
+                fval = -d**2 + conf%addition; fd = -fval
             else
                 z = a + goldenRatio*(d-a)
                 ! case 2 [a c d b] <--- [a z c d]
@@ -62,7 +97,7 @@ contains
                 d = c
                 fd = fc
                 c = z
-                fval = -c**2; fc = -fval
+                fval = -c**2 + conf%addition; fc = -fval
             endif
 
             gssDist = b - a
@@ -86,8 +121,14 @@ contains
         real(rk), intent(in) :: k
         class(*), intent(in), target, optional :: func_data
         real(rk) :: f
+        type(configurations) :: conf
 
-        f = -k**2
+        select type(func_data)
+            type is (configurations)
+            conf = func_data
+        end select
+
+        f = -k**2 + conf%addition
     end function f
 
 
